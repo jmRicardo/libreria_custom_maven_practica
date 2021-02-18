40a9704b9d691d56cce03456dc8573ff133831bc
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-18 14:10:16 -0300

    Initial commit

---
+++ src/main/java/com/jmr/practica/entities_practica/libreria_custom_maven_practica/LibreriaCustomMavenPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.entities_practica.libreria_custom_maven_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaCustomMavenPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaCustomMavenPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/practica/entities_practica/libreria_custom_maven_practica/LibreriaCustomMavenPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.entities_practica.libreria_custom_maven_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaCustomMavenPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
