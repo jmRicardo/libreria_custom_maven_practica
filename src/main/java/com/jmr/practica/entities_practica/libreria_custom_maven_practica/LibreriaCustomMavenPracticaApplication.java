27e98b52a31ef94478bbe1e5bd166301a176ac50
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-22 12:14:09 -0300

    eliminado metodo main y build de maven ( no necesarios )

--- src/main/java/com/jmr/practica/entities_practica/libreria_custom_maven_practica/LibreriaCustomMavenPracticaApplication.java
+++ src/main/java/com/jmr/practica/entities_practica/libreria_custom_maven_practica/LibreriaCustomMavenPracticaApplication.java
@@ -1,14 +1,8 @@
 package com.jmr.practica.entities_practica.libreria_custom_maven_practica;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LibreriaCustomMavenPracticaApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(LibreriaCustomMavenPracticaApplication.class, args);
-    }
-
 }
